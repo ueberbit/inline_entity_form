24949bfa223904cf7e4681d26c88bbba03d24bc5
Florian Weber <florian.weber@ueberbit.de>
2015-03-26 13:06:36 +0100

    Fix nested inline entity forms

--- src/Plugin/Field/FieldWidget/InlineEntityFormMultiple.php
+++ src/Plugin/Field/FieldWidget/InlineEntityFormMultiple.php
@@ -629,12 +629,9 @@ class InlineEntityFormMultiple extends WidgetBase implements ContainerFactoryPlu
 
     $this->initializeIefController();
     $field_name = $this->fieldDefinition->getName();
-    if (isset($form['#ief_parents'])) {
-      $parents = $form['#ief_parents'];
-    }
-    else {
-      $parents = array($field_name, 'form');
-    }
+
+    // Extract the values from $form_state->getValues().
+    $parents = array_merge($form['#parents'], array($field_name, 'form'));
     $ief_id = sha1(implode('-', $parents));
     $this->setIefId($ief_id);
 
